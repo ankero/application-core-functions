f32142be9a780012d4a7551f1c436d4aab6a29b6
Antero Hanhirova <ankero@MacBook-Pro.local>
2020-11-16 17:07:22 +0200

    fix error

--- functions/src/analytics.ts
+++ functions/src/analytics.ts
@@ -2,7 +2,6 @@ import * as functions from "firebase-functions";
 
 import { AUDIT_LOG_EVENTS } from "./constants";
 import { createAuditLogEvent } from "./services/auditLog";
-import { createOrUpdateProfile } from "./services/user";
 
 export const identifies = functions.https.onCall(async (data, context) => {
   try {
@@ -10,8 +9,6 @@ export const identifies = functions.https.onCall(async (data, context) => {
     if (!uid) {
       throw new Error("UNAUTHORIZED");
     }
-
-    await createOrUpdateProfile(uid, { lastSignedInMillis: Date.now() });
 
     await createAuditLogEvent({
       event: AUDIT_LOG_EVENTS.USER_SIGNED_IN,
